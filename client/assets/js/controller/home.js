dbe1a2f554286bb7201ff7a37ae13e4e39206ca7
Gevorg Harutyunyan <gevorg.ha@gmail.com>
2015-02-06 01:25:44 +0400

    Tracing..

--- client/assets/js/controller/home.js
+++ client/assets/js/controller/home.js
@@ -3,6 +3,8 @@ angular.module('TypeItQuick').
     controller('HomeCtrl', ['$scope', '$http', '$location', '$window', function($scope, $http, $location, $window) {
         // Render captcha.
         $($window).on('recaptchaLoaded', function() {
+            console.log('CAPTCHA LOADED!!!');
+            console.log(grecaptcha.render);
             grecaptcha.render('g-recaptcha', {
                 'sitekey': $window.captchaKey
             });
