883cdea0cc18f17f8d56ae0f0dba17a5aa8ed056
Gevorg Harutyunyan <gevorg.ha@gmail.com>
2015-02-06 01:32:21 +0400

    Tracing..

--- client/assets/js/controller/home.js
+++ client/assets/js/controller/home.js
@@ -1,6 +1,7 @@
 // Home controller for TypeItQuick app.
 angular.module('TypeItQuick').
     controller('HomeCtrl', ['$scope', '$http', '$location', '$window', function($scope, $http, $location, $window) {
+        console.log('CHAKA!!!');
         // Render captcha.
         $($window).on('recaptchaLoaded', function() {
             console.log('CAPTCHA LOADED!!!');
